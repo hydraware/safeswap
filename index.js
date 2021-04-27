35751670275ef8b60eef60d7eca6f1961ec72a7f
hydra <hydra@reft.io>
2021-04-27 19:27:41 -0400

    created initial commands
    -set bot calling prefix "ss".
    -directed to read .js files from the ./commands dir.
    -initialized default ping & pong commands scanning as per testing period.

--- index.js
+++ index.js
@@ -2,8 +2,34 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
 
+const prefix = 'ss ';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
 client.on('ready', () => {
     console.log('SafeSwap is now online!')
+})
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args);
+    } else if (command == 'pong'){
+        client.commands.get('pong').execute(message, args);
+    } 
 })
 
 client.login(config.token)
