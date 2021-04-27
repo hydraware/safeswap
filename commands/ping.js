8724c8ee1edc1668c06b449ce489f33cb26626d4
hydra <hydra@reft.io>
2021-04-27 19:52:12 -0400

    embedment of default commands
    -created embeds for the default ping & pong commands.

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,13 @@
+const { Client, MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: 'ping',
     description: 'this is a ping command!',
     execute(message, args){
-        message.channel.send('pong!');
+        const embed = new MessageEmbed()
+            .setTitle('Ping!')
+            .setColor(0x3395FC)
+            .setImage('https://i.imgur.com/8yvdujb.png');
+        message.channel.send(embed);
     }
 }
--- commands/pong.js
+++ commands/pong.js
@@ -1,7 +1,13 @@
+const { Client, MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: 'pong',
     description: 'this is a pong command!',
     execute(message, args){
-        message.channel.send('ping!');
+        const embed = new MessageEmbed()
+            .setTitle('Pong!')
+            .setColor(0x3395FC)
+            .setImage('https://i.imgur.com/8yvdujb.png');
+        message.channel.send(embed);
     }
 }
